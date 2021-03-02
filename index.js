7f9c7da6ced5929cc1022af5bf855461e11864e0
Yuping Zuo <zypA13510@users.noreply.github.com>
2021-03-02 14:02:32 +0800

    refactor!: use simpler call signature
    Simplify the calls required from 3 statements(new, question, and close)
    to 1. Also prevent forgetting to call close.
    
    BREAKING CHANGE: the way to call this library is changed from
    `(new MutePrompt()).question()` to `prompt()`.

--- index.js
+++ index.js
@@ -1,65 +1,47 @@
 'use strict';
 
 const ReadLine = require('readline');
-const Writable = require('stream').Writable;
+const WritableStream = require('stream').Writable;
 
-class MutePrompt {
-  constructor () {
-    this.stdout = this.createStdoutInterface();
-    this.unmuteStdout();
-    this.stdin = this.createStdinInterface(this.stdout);
+// prompt with a question
+async function question (query, muted) {
+  let stdout = createStdoutInterface();
+  let stdin = createStdinInterface(stdout);
+
+  if (muted) {
+    // show query before muting
+    stdout.write(query);
+    stdout.muted = true;
+  }
+  let response = await new Promise((resolve, reject) => stdin.question(query, response => resolve(response)));
+  if (muted) {
+    stdout.muted = false;
+    stdout.write('\n'); // add the muted new line
   }
 
-  // prompt with a question
-  async question (query, muted) {
-    if (muted) {
-      // show query before muting
-      this.stdout.write(query);
-      this.muteStdout();
-    }
-    let response = await new Promise((resolve, reject) => this.stdin.question(query, response => resolve(response)));
-    if (muted) {
-      this.unmuteStdout();
-      this.stdout.write('\n'); // add the muted new line
-    }
-    return response;
-  }
+  stdin.close();
+  stdout.end();
 
-  // create the stdin interface for the prompt
-  createStdinInterface (stdout) {
-    return ReadLine.createInterface({
-      input: process.stdin,
-      output: stdout,
-      terminal: true
-    });
-  }
-
-  // create the stdout interface for the prompt
-  createStdoutInterface () {
-    return new Writable({
-      write: function (chunk, encoding, callback) {
-        if (!this.muted) process.stdout.write(chunk, encoding);
-        callback();
-      }
-    });
-  }
-
-  // mute stdout output
-  muteStdout () {
-    this.stdout.muted = true;
-  }
-
-  // unmute stdout output
-  unmuteStdout () {
-    this.stdout.muted = false;
-  }
-
-  close () {
-    this.stdin.close();
-    this.stdin = null;
-    this.stdout.end();
-    this.stdout = null;
-  }
+  return response;
 }
 
-module.exports = MutePrompt;
+// create the stdin interface for the prompt
+function createStdinInterface (stdout) {
+  return ReadLine.createInterface({
+    input: process.stdin,
+    output: stdout,
+    terminal: true
+  });
+}
+
+// create the stdout interface for the prompt
+function createStdoutInterface () {
+  return new WritableStream({
+    write: function (chunk, encoding, callback) {
+      if (!this.muted) process.stdout.write(chunk, encoding);
+      callback();
+    }
+  });
+}
+
+module.exports = question;
--- test.js
+++ test.js
@@ -1,24 +1,22 @@
-let MutedPrompt = require('./index.js');
+let prompt = require('./index.js');
 
-askQuestions(new MutedPrompt())
-  .then((answers) => {
-    answers.forEach((answer, i) => {
-      console.log('Answer ' + i + ': ' + answer);
-    });
-  })
-  .catch((error) => {
-    console.log(error.stack);
-    process.exitCode = 1;
+(async function test() {
+  let answers = await askQuestions();
+
+  answers.forEach((answer, i) => {
+    console.log(`Answer ${i}: ${answer}`);
   });
+})().catch((error) => {
+  console.log(error.stack);
+  process.exitCode = 1;
+});
 
-async function askQuestions(prompt) {
+async function askQuestions() {
   console.log('Four questions with alternating muted prompt...');
-  let returnValue = [
-    await prompt.question("(unmuted) Question 1? "),
-    await prompt.question("(muted)   Question 2? ", true),
-    await prompt.question("(unmuted) Question 3? "),
-    await prompt.question("(muted)   Question 4? ", true)
+  return [
+    await prompt("(unmuted) Question 1? "),
+    await prompt("(muted)   Question 2? ", true),
+    await prompt("(unmuted) Question 3? "),
+    await prompt("(muted)   Question 4? ", true)
   ];
-  prompt.close();
-  return returnValue;
 }
